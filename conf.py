a0168b9fe835395c6fd8551c7489349b53243a9b
XiaoboCui <cuixiaobo0223@foxmail.com>
2023-02-19 17:01:21 +0800

    remove sphinx_markdown_tables extension

--- conf.py
+++ conf.py
@@ -32,8 +32,7 @@ release = '1.5'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'recommonmark',
-    'sphinx_markdown_tables'
+    'recommonmark'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
